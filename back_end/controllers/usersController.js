be62fde48b1c8011b5944def735b14a975c94ca0
Ryan <ryan.m.tan95@gmail.com>
2020-09-25 13:37:31 +0800

    modify userscontroller

--- back_end/controllers/usersController.js
+++ back_end/controllers/usersController.js
@@ -6,8 +6,6 @@ const url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com";
 
 class UsersController {
 
-
-
     static login(req, res) {
         const { username, password } = req.body;
         const params = { username: username, password: password }
@@ -17,13 +15,14 @@ class UsersController {
                 const authorization_token = result.data
                 const user = new User(username, authorization_token)
                 saveUser(user)
-                res.status(400).send({ login_status: true });
+                res.status(200).send({ login_status: true });
             })
             .catch((err) => {
-                console.log(err)
-                res.status(404).send({ login_status: false })
+                res.status(200).send({ login_status: false })
             })
+
     }
+
 }
 
 module.exports = UsersController
