f4e2008282f3267289925700eab6785ab1b859ae
Ryan <ryan.m.tan95@gmail.com>
2020-09-25 12:07:49 +0800

    set up redis properly

--- back_end/app.js
+++ back_end/app.js
@@ -8,6 +8,19 @@ const PORT = 3000;
 const app = express();
 const server = http.createServer(app);
 
+const redis = require("redis");
+let port = 6379
+let host = "127.0.0.1"
+const client = redis.createClient(port, host);
+
+client.on('connect', function () {
+    console.error('Redis client connected');
+});
+
+client.on('error', function (err) {
+    console.error('Something went wrong ' + err);
+});
+
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
--- back_end/models/user.js
+++ back_end/models/user.js
@@ -1,16 +1,3 @@
-const redis = require("redis");
-let port = 6379
-let host = "127.0.0.1"
-const client = redis.createClient(port, host);
-
-client.on('connect', function () {
-    console.error('Redis client connected');
-});
-
-client.on('error', function (err) {
-    console.error('Something went wrong ' + err);
-});
-
 class User {
     constructor(username, authorization_token) {
         this.username = username;
