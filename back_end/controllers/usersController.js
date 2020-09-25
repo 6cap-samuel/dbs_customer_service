efd4f874f694f8b08c66c873b16b64409fc7b260
Ryan <ryan.m.tan95@gmail.com>
2020-09-25 12:54:57 +0800

    modify user controller

--- back_end/controllers/usersController.js
+++ back_end/controllers/usersController.js
@@ -1,15 +1,27 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const axios = require('axios');
+const { saveUser } = require('../models/user')
 
-const url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com"
+const url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com";
 
 class UsersController {
 
 
 
     static login(req, res) {
-        const { username, password } = req.body
-        console.log(username, password)
-        res.status(400).json({ hello: "hello" })
+        const { username, password } = req.body;
+        const params = { username: username, password: password }
+
+        axios.post(url + '/login', params)
+            .then((result) => {
+                const authorization_token = result.data
+                console.log(result.data)
+                res.status(400).send({ login_status: true });
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(404).send({ error: "error help" })
+            })
     }
 }
 
